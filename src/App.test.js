101d0c0b9919d409cc387852fce81158eb7ac0b2
Kevin Gonzalez <typekev@gmail.com>
2020-03-18 20:58:14 +0100

    Fix basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("tests general render", () => {
+  test("renders generate block button", () => {
+    const { getByText } = render(<App />);
+    const buttonElement = getByText(/generate block/i);
+    expect(buttonElement).toBeInTheDocument();
+  });
 });
