b96ca39c7055211428946157e4002aed66a3c651
Kevin Gonzalez <typekev@gmail.com>
2020-03-22 21:16:02 +0100

    style prettyJSON

--- src/components/PrettyJSONView.js
+++ src/components/PrettyJSONView.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { css } from "emotion";
 
-const PrettyJSONView = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+const PrettyJSONView = ({ data, name }) => (
+  <pre
+    className={css`
+      margin: 0;
+      height: 100%;
+      overflow: scroll;
+    `}
+  >
+    ➜ react-blockchain-examples git:(master) ✗{" "}
+    {`< ${name}_blockchain_data.json`}
+    <br />
+    {JSON.stringify(data, null, 2)}
+    <br />
+    (END)
+  </pre>
+);
 
 export default PrettyJSONView;
